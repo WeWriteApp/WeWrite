fa8c67eb51256df94a6fd3a615e58870b86b63fb
Fliko <baxter.flw@gmail.com>
2024-08-15 18:54:37 -0700

    configure through env

---
+++ app/firebase/config.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "envy";
+
+const {
+    API_KEY,
+    DOMAIN,
+    DB_URL,
+    PID,
+    BUCKET,
+    MSNGR_ID,
+    APP_ID,
+} =  envy();
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const newConfig =  {
+  apiKey: API_KEY,
+  authDomain: DOMAIN,
+  databaseURL: DB_URL,
+  projectId: PID,
+  storageBucket: BUCKET,
+  messagingSenderId: MSNGR_ID,
+  appId: APP_ID,
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(newConfig);
