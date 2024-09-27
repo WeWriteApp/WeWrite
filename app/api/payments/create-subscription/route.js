684480474e3c9bd4b76d4a1f3c340c364cc64baa
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2024-09-27 17:32:21 -0400

    Fix for deployment

--- app/api/payments/create-subscription/route.js
+++ app/api/payments/create-subscription/route.js
@@ -1,21 +1,13 @@
 import { NextResponse } from 'next/server';
-import Stripe from 'stripe';
 
-const stripe = new Stripe("123", { apiVersion: '2023-08-16' });
 
 export async function POST(request) {
   const { amount, currency } = await request.json();
 
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100, // Amount in cents (e.g., $300 -> 30000)
-      currency: currency || 'usd',
-      payment_method_types: ['card'], // Optionally extend this based on available methods
-    });
+    // return test route
 
-    return NextResponse.json({
-      clientSecret: paymentIntent.client_secret,
-    });
+    return NextResponse.json({ amount, currency });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
