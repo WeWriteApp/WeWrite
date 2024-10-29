c34830fdf47572ec5444e346412283ca8bbd61e5
asseph <davidkano.dk@gmail.com>
2024-10-30 03:52:17 +0900

    add: group, data provider

---
+++ src/providers/DataProvider.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { createContext, ReactNode, useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+import usePages from "@/hook/usePages";
+
+interface DataContextType {
+  pages?: any;
+  loadMorePages?: any;
+  isMoreLoading?: any;
+  hasMorePages?: boolean
+}
+
+interface DataProviderProps {
+  children: ReactNode;
+}
+
+export const DataContext = createContext<DataContextType>({});
+
+export const DataProvider: React.FC<DataProviderProps> = ({ children }) => {
+  const { user } = useContext(AuthContext); // Get the authenticated user
+  // Use the usePages hook, passing in the userId if the user is authenticated
+  const {
+    pages,
+    loadMorePages,
+    isMoreLoading,
+    hasMorePages
+  } = usePages(user ? user.uid : null); // Use `user.uid` to fetch pages for the logged-in user
+
+  // Optionally: Handle filtered pages if needed
+
+  return (
+    <DataContext.Provider
+      value={{
+        pages,
+        loadMorePages,
+        isMoreLoading,
+        hasMorePages,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
---
+++ src/providers/GroupsProvider.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useState, useEffect, ReactNode, createContext } from "react";
+import { rtdb } from "../firebase/rtdb";
+import { onValue, ref } from "firebase/database";
+
+
+interface GroupsContextType {
+  groups?: any;
+}
+
+interface GroupsProviderProps {
+  children: ReactNode;
+}
+
+export const GroupsContext = createContext<GroupsContextType>({});
+
+export const GroupsProvider: React.FC<GroupsProviderProps> = ({ children }) => {
+
+
+  const [groups, setGroups] = useState<any>([]);
+
+  useEffect(() => {
+    const groupsRef = ref(rtdb, "groups");
+    return onValue(groupsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const arr = Object.keys(data).map((key) => ({
+          id: key,
+          ...data[key],
+        }));
+        setGroups(arr);
+      } else {
+        setGroups([]);
+      }
+
+    });
+  }, []);
+
+  return (
+    <GroupsContext.Provider value={groups}>
+      {children}
+    </GroupsContext.Provider>
+  );
+}
