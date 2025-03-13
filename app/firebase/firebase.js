e567e1a9813ac47a601afcbd94e274c69ed6be41
WeWrite App <jamiegray2234@gmail.com>
2025-03-12 23:40:32 -0400

    fix: remove duplicate useFirebase export
    - Remove duplicate export of useFirebase hook\n- Keep single export from FirebaseContext

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -2,9 +2,6 @@
 
 export { FirebaseProvider, useFirebase } from '../context/FirebaseContext';
 
-// Re-export the hook for convenience
-export { useFirebase };
-
 // For backward compatibility, export null values by default
 export const firebase_app = null;
 export const rtdb = null;
