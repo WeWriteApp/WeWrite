9c1ba4eb91683b4136ad56defddd96b43f3923d9
WeWrite App <jamiegray2234@gmail.com>
2025-03-15 00:05:05 -0400

    refactor: move dashboard from /pages to root level

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+import Dashboard from "./components/Dashboard";
+
+export async function generateMetadata() {
+  return {
+    title: "Your WeWrite",
+    description: "Your WeWrite dashboard",
+  };
+}
+
 export default function Home() {
   return (
-    <>
-      {/* <Header /> */}
-    </>
+    <Dashboard />
   );
 }
