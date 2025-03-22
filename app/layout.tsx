fc8fdd148f38f0bd24f3414d0176288d6812c751
WeWrite App <jamiegray2234@gmail.com>
2025-03-22 15:40:31 -0400

    Fix more build issues: Update import paths in layout.tsx, add styled-components dependency, create missing textarea component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/providers/ThemeProvider"
-import { AuthProvider } from "@/providers/AuthProvider"
-import { AppProvider } from "@/providers/AppProvider"
-import { DataProvider } from "@/providers/DataProvider"
+import { ThemeProvider } from "./providers/ThemeProvider"
+import { AuthProvider } from "./providers/AuthProvider"
+import { AppProvider } from "./providers/AppProvider"
+import { DataProvider } from "./providers/DataProvider"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import GAProvider from "@/providers/GAProvider"
-import { AnalyticsProvider } from "@/providers/AnalyticsProvider"
+import GAProvider from "./providers/GAProvider"
+import { AnalyticsProvider } from "./providers/AnalyticsProvider"
 import dynamic from "next/dynamic"
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "./components/ui/toaster"
 
 const ClientLayout = dynamic(() => import("./ClientLayout"), { ssr: true })
 
