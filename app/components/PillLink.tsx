fc974fbb942e5d63f709ac021c400507936ce48b
WeWrite App <jamiegray2234@gmail.com>
2025-03-28 16:11:17 -0400

    feat: create PillLink component with required props
    - Added PillLink.tsx with all required props to fix TypeScript errors
    - This should allow the build to proceed

---
+++ app/components/PillLink.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type PillLinkProps = {
+  children: React.ReactNode;
+  href: string;
+  isPublic: boolean;
+  groupId?: string;
+  className?: string;
+  isOwned?: boolean;
+  byline?: string;
+  isLoading?: boolean;
+  variant?: string;
+  label?: string;
+};
+
+const PillLink = ({
+  children,
+  href,
+  isPublic,
+  groupId = '',
+  className = '',
+  isOwned = false,
+  byline = '',
+  isLoading = false,
+  variant = 'default',
+  label = ''
+}: PillLinkProps) => {
+  return (
+    <a href={href} className={`${className} pill-link`}>
+      {children}
+    </a>
+  );
+};
+
+export default PillLink;
